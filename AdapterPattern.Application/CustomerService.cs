92ddefac6e7ba97aa1efa1e1984a5f0c833162ec
Tiago Pariz <tiagopariz@gmail.com>
2017-09-02 20:08:00 -0300

    Camada de aplicação

---
+++ AdapterPattern.Application/CustomerService.cs
@@ -0,0 +1,31 @@
+﻿using AdapterPattern.Domain.Entities;
+using AdapterPattern.Domain.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Web;
+
+namespace AdapterPattern.Application
+{
+    public class CustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public IList<Customer> GetAllCustomers()
+        {
+            IList<Customer> customers;
+            string storageKey = "GetAllCustomers";
+            customers = (List<Customer>) HttpContext.Current.Cache.Get(storageKey);
+            if (customers == null)
+            {
+                customers = _customerRepository.GetCustomers();
+                HttpContext.Current.Cache.Insert(storageKey, customers);
+            }
+
+            return customers;
+        }
+    }
+}
