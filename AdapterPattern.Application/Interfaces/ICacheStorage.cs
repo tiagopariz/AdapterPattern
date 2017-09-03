c051343420b337a29a2867f7b04bce9815019b80
Tiago Pariz <tiagopariz@gmail.com>
2017-09-02 21:49:58 -0300

    Interface de cache

---
+++ AdapterPattern.Application/Interfaces/ICacheStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace AdapterPattern.Application.Interfaces
+{
+    public interface ICacheStorage
+    {
+        void Remove(string key);
+        void Store(string key, object data);
+        T Retrieve<T>(string key);
+    }
+}
